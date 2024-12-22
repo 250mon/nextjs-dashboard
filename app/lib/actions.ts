75f3c7957b48cf2e32788d21aed61467a8ff8574
Jye Young Song <lambki@naver.com>
2024-12-22 20:27:15 +0900

    upto ch14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,25 +5,49 @@ import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer",
+  }),
+  amount: z.coerce.number().gt(0, { message: "Amount must be greater than 0" }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select a status",
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  // validate form fields using Zod
+  const validateFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+  // if validation fails, return the errors early
+  if (!validateFields.success) {
+    return {
+      errors: validateFields.error.flatten().fieldErrors,
+      message: "Validation Error: Failed to create invoice.",
+    };
+  }
+
+  const { customerId, amount, status } = validateFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
+  // Insert the invoice into the database
   try {
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
@@ -35,6 +59,7 @@ export async function createInvoice(formData: FormData) {
     };
   }
 
+  // Revalidate the cache and redirect to the invoices page
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
